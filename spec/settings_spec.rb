6dbaaffbe9d72b9bb40e17de9d301836642eb55b
Juan C. Müller <jcmuller@gmail.com>
2012-08-13 17:58:16 -0400

    Test more types of YAML data types

--- spec/settings_spec.rb
+++ spec/settings_spec.rb
@@ -91,9 +91,9 @@ describe Figleaf::Settings do
     }
 
     before do
-      Dir.should_receive(:glob).and_return(["config/described_class/some_service.yml"])
-      described_class.should_receive(:env).and_return("test")
-      YAML.should_receive(:load_file).and_return(configuration)
+      Dir.stub(:glob).and_return(["config/described_class/some_service.yml"])
+      described_class.stub(:env).and_return("test")
+      YAML.stub(:load_file).and_return(configuration)
 
       described_class.load_settings
     end
@@ -117,6 +117,36 @@ describe Figleaf::Settings do
 
     it "should create bool_false? and return false" do
       described_class.some_service.bool_false?.should be_false
+    end
+
+    it "should work for arrays as well" do
+      YAML.stub(:load_file).and_return({ "test" => [1, 2] })
+      described_class.load_settings
+      described_class.some_service.should == [1, 2]
+    end
+
+    it "and for plain strings" do
+      YAML.stub(:load_file).and_return({ "test" => "Hello, World!" })
+      described_class.load_settings
+      described_class.some_service.should == "Hello, World!"
+    end
+
+    it "and for booleans (true)" do
+      YAML.stub(:load_file).and_return({ "test" => true })
+      described_class.load_settings
+      described_class.some_service.should be_true
+    end
+
+    it "and for booleans (false)" do
+      YAML.stub(:load_file).and_return({ "test" => false })
+      described_class.load_settings
+      described_class.some_service.should be_false
+    end
+
+    it "should raise exception when loading an undefined value" do
+      YAML.stub(:load_file).and_return({ "test" => false })
+      described_class.load_settings
+      expect { described_class.some_services }.to raise_error NoMethodError
     end
 
   end
