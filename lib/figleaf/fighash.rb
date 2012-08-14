b2df4744fcf2af36f5162d5216c5f5c3f62d18f4
Juan C. Müller <jcmuller@gmail.com>
2012-08-14 13:48:28 -0400

    Force raising error when method doesn't have a corresponding key

--- lib/figleaf/fighash.rb
+++ lib/figleaf/fighash.rb
@@ -1,7 +1,14 @@
 module Figleaf
-  class Fighash < Hash
-    include Hashie::Extensions::IndifferentAccess
-    include Hashie::Extensions::MergeInitializer
-    include Hashie::Extensions::MethodAccess
+  class Fighash < Hashie::Mash
+    def method_missing(method_name, *args, &blk)
+      return super if key?(method_name)
+      case method_name
+      when /.*?[?=!]$/
+        super(method_name, *args, &blk)
+      else
+        raise NoMethodError
+      end
+    end
+
   end
 end
