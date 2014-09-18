dc1ae68a256dbdeb042c690d5b1f48e33f613d15
Ross Kaffenberger <rosskaff@gmail.com>
2014-09-18 15:27:29 -0400

    rename fixtures

--- spec/figleaf/settings_spec.rb
+++ spec/figleaf/settings_spec.rb
@@ -9,51 +9,51 @@ describe Figleaf::Settings do
     end
 
     it "should load some service" do
-      described_class.some_service["foo"].should == "bar"
+      described_class.service["foo"].should == "bar"
     end
 
     it "should load indifferently the key names" do
-      described_class.some_service["foo"].should == "bar"
-      described_class.some_service[:foo].should == "bar"
+      described_class.service["foo"].should == "bar"
+      described_class.service[:foo].should == "bar"
     end
 
     it "should create foo as a method" do
-      described_class.some_service.foo.should == "bar"
+      described_class.service.foo.should == "bar"
     end
 
     it "should create bool_true? and return true" do
-      described_class.some_service.bool_true?.should be_true
+      described_class.service.bool_true?.should be_true
     end
 
     it "should create bool_false? and return false" do
-      described_class.some_service.bool_false?.should be_false
+      described_class.service.bool_false?.should be_false
     end
 
-    it "should work for arrays as well" do
+    it "should work for array as well" do
       described_class.load_settings
-      described_class.arrays.should == [1, 2]
+      described_class.array.should == [1, 2]
     end
 
-    it "and for plain strings" do
+    it "and for plain string" do
       described_class.load_settings
-      described_class.strings.should == "Hello, World!"
+      described_class.string.should == "Hello, World!"
     end
 
-    it "and for booleans (true)" do
+    it "and for boolean (true)" do
       described_class.load_settings
-      described_class.booleans.should be_true
+      described_class.boolean.should be_true
     end
 
-    it "and for booleans (false)" do
+    it "and for boolean (false)" do
       described_class.load_settings(@fixtures_path, "alt")
       described_class.load_settings
-      described_class.some_service.should be_false
+      described_class.service.should be_false
     end
 
     it "should raise exception when loading an undefined value" do
       YAML.stub(:load_file).and_return({ "test" => {} })
       described_class.load_settings
-      expect { described_class.some_service.blah }.to raise_error NoMethodError
+      expect { described_class.service.blah }.to raise_error NoMethodError
     end
 
   end
